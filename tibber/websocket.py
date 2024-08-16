18e961dea5998cfb6cfa05017cfec38f572896e7
Martin Hjelmare <marhje52@gmail.com>
2024-08-16 21:36:53 +0200

    Move websocket transport

--- tibber/websocket.py
+++ tibber/websocket.py
@@ -8,10 +8,11 @@ from ssl import SSLContext
 from typing import Any
 
 from gql import Client
+from gql.transport.exceptions import TransportClosed
+from gql.transport.websockets import WebsocketsTransport
 
 from .exceptions import SubscriptionEndpointMissingError
 from .home import TibberHome
-from .websocket_transport import TibberWebsocketsTransport
 
 LOCK_CONNECT = asyncio.Lock()
 
@@ -203,3 +204,40 @@ class TibberRT:
         self._sub_endpoint = sub_endpoint
         if self.sub_manager is not None and isinstance(self.sub_manager.transport, TibberWebsocketsTransport):
             self.sub_manager.transport.url = sub_endpoint
+
+
+class TibberWebsocketsTransport(WebsocketsTransport):
+    """Tibber websockets transport."""
+
+    def __init__(self, url: str, access_token: str, user_agent: str, ssl: SSLContext | bool = True) -> None:
+        """Initialize TibberWebsocketsTransport."""
+        super().__init__(
+            url=url,
+            init_payload={"token": access_token},
+            headers={"User-Agent": user_agent},
+            ping_interval=30,
+            ssl=ssl,
+        )
+        self._user_agent: str = user_agent
+        self._timeout: int = 90
+        self.reconnect_at: dt.datetime = dt.datetime.now(tz=dt.UTC) + dt.timedelta(seconds=self._timeout)
+
+    @property
+    def running(self) -> bool:
+        """Is real time subscription running."""
+        return self.websocket is not None and self.websocket.open
+
+    async def _receive(self) -> str:
+        """Wait the next message from the websocket connection."""
+        try:
+            msg = await asyncio.wait_for(super()._receive(), timeout=self._timeout)
+        except TimeoutError:
+            _LOGGER.error("No data received from Tibber for %s seconds", self._timeout)
+            raise
+        self.reconnect_at = dt.datetime.now(tz=dt.UTC) + dt.timedelta(seconds=self._timeout)
+        return msg
+
+    async def close(self) -> None:
+        """Close the websocket connection."""
+        await self._fail(TransportClosed(f"Tibber websocket closed by {self._user_agent}"))
+        await self.wait_closed()
